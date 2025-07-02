1f702c73fc1a2ec115bdcd928a99a56a643d7a5b
Momen-Abdelhaleem <momenelharoon23@gmail.com>
2025-07-02 21:40:05 +0300

    add hive Id and type field to note model and auto generate note model.g which called build runner

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,15 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
   NoteModel({
     required this.title,
@@ -13,4 +21,10 @@ class NoteModel {
 // hive_flutter   بتخليك تستعملها في فلاتر لان hive من الباكدج اللي اسمها extention هي عبارة عن 
 // باكدج بتاعة دارت hive اصلا 
 // build runner للكود generate  و بستعملها عشان تعمل  dev_dependencies بكتبها جوا ال 
-
+//hive key & value بتخزن الكود على هيئة 
+//عشان اخزن فية البيانات و ممكن انشئ اكتر من واحد عادي box بننشئ   hive  في 
+//,لازم اعمل التالي object و عشان اخزن جواها  premitive data type بتخزن  hive
+//عشان يعمل الحتة دي hive generator  و كدة كدة احنا عاملين  type adapter بنشئ حاجة اسمها 
+//hive object لحاجة اسمها  extend  يعمل  model  بخلي ال  
+// و الارقام بتزيد مع كل متغير @hiveField(0) و بديلة رقم خاص بيه كدة unique field بعمل 
+//flutter packages pub run build_runner build بكتب في التيرمنال 
