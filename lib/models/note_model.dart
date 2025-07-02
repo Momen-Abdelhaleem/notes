e554ed957bf70ffcc4693c705744964d867b1748
Momen-Abdelhaleem <momenelharoon23@gmail.com>
2025-07-02 18:44:55 +0300

    add Hive

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -10,3 +10,7 @@ class NoteModel {
     required this.color,
   });
 }
+// hive_flutter   بتخليك تستعملها في فلاتر لان hive من الباكدج اللي اسمها extention هي عبارة عن 
+// باكدج بتاعة دارت hive اصلا 
+// build runner للكود generate  و بستعملها عشان تعمل  dev_dependencies بكتبها جوا ال 
+
